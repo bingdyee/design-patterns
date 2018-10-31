bb13efe721796e8b9e483e6a58001da2e96a94f9
fetaxyu <fetaxyu@gmail.com>
2018-10-31 14:02:32 +0800

    factory method

---
+++ design_patterns/factory_method/factory.cc
@@ -0,0 +1,14 @@
+/* Copyleft 2018 The design-patterns Authors. All Rights Reserved.
+
+工厂方法
+
+@Author: fetaxyu
+@Date: 2018-10-31
+Email: fetaxyu@gmail.com
+===============================================================*/
+
+#include "design_patterns/factory_method/factory.h"
+
+namespace designs {
+
+}
---
+++ design_patterns/factory_method/factory.h
@@ -0,0 +1,17 @@
+/* Copyleft 2018 The design-patterns Authors. All Rights Reserved.
+
+工厂方法
+
+@Author: fetaxyu
+@Date: 2018-10-31
+Email: fetaxyu@gmail.com
+===============================================================*/
+#ifndef DESIGN_PATTERNS_FACTORY_H_
+#define DESIGN_PATTERNS_FACTORY_H_
+
+namespace designs {
+
+}
+
+
+#endif
